5768b18adb046e3fa643c697c3b36cbce6449fdc
REEMAALHUDAIAN <reemaalhudaian@gmail.com>
2024-09-29 12:47:33 +0300

    Initial Commit

---
+++ quiz/quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizApp.swift
+//  quiz
+//
+//  Created by Reema ALhudaian on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ quizTests/quizTests.swift
@@ -0,0 +1,17 @@
+//
+//  quizTests.swift
+//  quizTests
+//
+//  Created by Reema ALhudaian on 26/03/1446 AH.
+//
+
+import Testing
+@testable import quiz
+
+struct quizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
